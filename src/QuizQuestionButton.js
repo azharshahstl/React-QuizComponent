5707bb8c17b91e55dbe6c560e7548cdd0ca47b13
Azhar Shah <azharshahsaintlouis@gmail.com>
2021-06-18 10:41:56 -0500

    Adding clickHandlers

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 
 class QuizQuestionButton extends Component {
 
+    handleClick = (button_text) => {
+        this.props.clickHandler(button_text)
+
+    }
+
     render() {
         return(
             <li>
-                <button>{this.props.button_text}</button>
+                <button onClick={this.handleClick}>{this.props.button_text}</button>
             </li>
         )
     }
